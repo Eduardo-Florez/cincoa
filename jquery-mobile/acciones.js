efd36983bf1004da6a373ad0ecabbc4ebc4c3d0d
Eduardo-Florez <damcb164e16@gmail.com>
2015-05-24 13:47:38 -0500

    6subida

--- jquery-mobile/acciones.js
+++ jquery-mobile/acciones.js
@@ -1,30 +1,32 @@
 // JavaScript Document
 
 $(document).ready(function(e){
-documnet.addEventListener("deviceready",function(){
-	
-$('#izquierda').on("swipeleft",function(){
-	navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion5","Aceptar"});	
-});//barrer izquierda
-
-
-$('#derecha').on("swipeleft",function(){
-	navigator.notification.alert("¿Que quieres hacer?",function(opt){	
-	switch(opt)
-	{
+	document.addEventListener("deviceready",function(){
 		
-	case 1:
-	navigator.notification.beep(1);	
-	break;
-	
-	case 2:
-	navigator.notification.vibrate(1000);	
-	break;
-	}//switch opt
-	},"Aplicacion5","Beep,Vibrar,Cancelar");//confirm
-	
-});//barrer derecha
-
-
-},false);//ready device
+		$('#izquierda').on("swipeleft", function(){
+			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion5","Aceptar"});
+		});//barrer izquierda
+		
+		
+		$('#derecha').on("swiperight", function(){
+			navigator.notification.confirm("¿Que quieres hacer?",function(opt){switch(opt)
+			{
+				
+				case 1:
+				navigator.notification.beep(1);
+				break;
+				
+				
+				case 2:
+				navigator.notification.vibrate(1000);
+				break;
+			}//switch opt
+			},"Aplicacion5","Beep,Vibrar,Cancelar");//confirm
+			
+		});//barrer derecha
+		
+		
+	},false);//ready device
 });//document
+		
+		
